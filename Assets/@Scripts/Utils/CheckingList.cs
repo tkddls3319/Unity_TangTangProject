c10b443215f612bc3ce81015b19aede60967254c
tkddls3319 <54829486+tkddls3319@users.noreply.github.com>
2023-09-14 16:57:25 +0900

    2023.09.14
    체킹리스트 업데이트

--- Assets/@Scripts/Utils/CheckingList.cs
+++ Assets/@Scripts/Utils/CheckingList.cs
@@ -5,20 +5,9 @@ using UnityEngine;
 public class CheckingList 
 {
     /*
-     * Player 애니메이션 추가  = OK
-     * ( 피격 모션, 죽는 모션) = OK
-     * Player 체력바 생성 = OK
-     * Player 데미지  = OK
-     * 
-     * 좀비 애니메이션 추가  OK
-     * (걷기)
-     * 
-     * 아이템 먹기 ok
-     * 아이탬 추가 획득 추가 ok
-     * 경험치바생성 및 경험치 획득ㅇ
-     * dropItem 추가
-     * 골드바 생성
-     * 
-     * 경험치100프로일때 래벨업
+     * hp포션 생성 
+     * 타이틀 골드랑 다이아 연동
+     * 경험치100프로일때 레벨업
+     * 스킬 북 만들기
      */
 }
